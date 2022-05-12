47b28c92d881b659ac613846b031ca2ee41f37b0
Destiny Frith <desilyvahn@gmail.com>
2022-05-12 14:19:00 -0500

    fixtures created

--- gamerraterapi/models/game.py
+++ gamerraterapi/models/game.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Game(models.Model):
     title = models.CharField(max_length=55)
     description = models.CharField(max_length=55)
@@ -8,3 +9,4 @@ class Game(models.Model):
     year_released = models.IntegerField()
     time_to_play = models.CharField(max_length=50)
     age_recommendation = models.IntegerField()
+    gamer = models.ForeignKey("Gamer", null=True, on_delete=models.CASCADE)
