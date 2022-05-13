89ed3c055b4ec0913aa181859f54e872f1d8b249
Destiny Frith <desilyvahn@gmail.com>
2022-05-13 13:53:27 -0500

    gameview added

--- gamerrater/urls.py
+++ gamerrater/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from django.conf.urls import include
 from django.urls import path
+from django.conf.urls import include
 from gamerraterapi.views import register_user, login_user
+from rest_framework import routers
+from gamerraterapi.views import GameView
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+router.register(r'games', GameView, 'game')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
---
+++ gamerraterapi/views/game.py
@@ -0,0 +1,46 @@
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from gamerraterapi.models import Game
+
+
+class GameView(ViewSet):
+    """Level up games view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single game
+
+        Returns:
+            Response -- JSON serialized game
+        """
+
+        try:
+            game = Game.objects.get(pk=pk)
+            serializer = GameSerializer(game)
+            return Response(serializer.data)
+        except Game.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        """Handle GET requests to get all games
+
+        Returns:
+            Response -- JSON serialized list of games
+        """
+
+        games = Game.objects.all()
+        # many=True since we are getting multiple objects back
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data)
+
+
+class GameSerializer(serializers.ModelSerializer):
+    """JSON serializer for games - converts data to JSON for client side
+    """
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'description', 'number_of_players',
+                  'designer', 'year_released', 'time_to_play', 'age_recommendation', 'gamer',)
+        depth = 1
+        # The Meta class hold the configuration for the serializer.
+        # We’re telling the serializer to use the Game model and to include all fields
