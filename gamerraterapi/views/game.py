126bf96f86947328150ad1dd1fc52f71b557abb9
Destiny Frith <desilyvahn@gmail.com>
2022-06-01 15:47:04 -0500

    added tests for game view

--- gamerraterapi/views/game.py
+++ gamerraterapi/views/game.py
@@ -45,6 +45,23 @@ class GameView(ViewSet):
         serializer.save(gamer=gamer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        game = Game.objects.get(pk=pk)
+        serializer = CreateGameSerializer(game, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        game = Game.objects.get(pk=pk)
+        game.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
 
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for games - converts data to JSON for client side
